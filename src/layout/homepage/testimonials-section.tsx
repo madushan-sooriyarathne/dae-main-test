b7302ccbea5d598a88464e6fec4889323dd81989
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-01-24 17:17:38 +0530

    feat: create testimonils section

---
+++ src/layout/homepage/testimonials-section.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  testimonials: Testimonial[];
+}
+
+const TestimonialSection: React.FC<Props> = ({
+  testimonials,
+}: Props): JSX.Element => {
+  return <div></div>;
+};
+
+export { TestimonialSection };
