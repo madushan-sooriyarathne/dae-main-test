d6e2f11d2c1fe3c7415886fe5341cad2558e54fd
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-01-27 15:56:26 +0530

    chor: removed unused code

--- src/hooks/fetch/useGetBrand.ts
+++
@@ -1,6 +0,0 @@
-import { getBrandData } from "@cms/content-studio";
-import { useQuery } from "@tanstack/react-query";
-
-export const useGetBrand = () => {
-  return useQuery(["brand"], async () => getBrandData());
-};
