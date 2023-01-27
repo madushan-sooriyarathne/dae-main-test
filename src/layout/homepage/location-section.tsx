99ccc1bf57c26543d6c5315c698ff234c9f90a55
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-01-27 15:54:54 +0530

    feat: create location-section

---
+++ src/layout/homepage/location-section.tsx
@@ -0,0 +1,31 @@
+import { ContentGroup } from "@layout/common/groups/content-group";
+
+import { ImageComponent } from "@components/image-component";
+
+import type { ContentGroupType } from "@layout/common/groups/content-group";
+
+interface Props extends ContentGroupType {
+  image: Image;
+}
+
+const LocationSection: React.FC<Props> = ({
+  image,
+  ...contentGroupProps
+}: Props): JSX.Element => {
+  return (
+    <div className=" mlg:main-grid-columns isolate grid w-full auto-rows-min grid-cols-1 mlg:bg-water-100">
+      <div className="-z-10  aspect-square w-full bg-water-100 md:aspect-video mlg:col-[col-start_4/_full-end] mlg:row-start-1 mlg:aspect-auto mlg:h-full 2xl:aspect-square 2xl:h-auto">
+        <ImageComponent
+          image={image}
+          objectFit="contain"
+          pos={{ x: "right", y: "center" }}
+        />
+      </div>
+      <div className="flex flex-col items-start justify-center px-4 py-9 mlg:col-[content-start_/_col-end_4] mlg:row-start-1 mlg:px-0 mlg:py-12">
+        <ContentGroup {...contentGroupProps} />
+      </div>
+    </div>
+  );
+};
+
+export { LocationSection };
