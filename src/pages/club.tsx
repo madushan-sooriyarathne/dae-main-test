4c99da1b1808aff84554c634bf0b91a214acae5e
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-01-28 18:35:42 +0530

    feat: add /club page

---
+++ src/pages/club.tsx
@@ -0,0 +1,102 @@
+import {
+  getFAQGroup,
+  getImageContentBlock,
+  getMultiImageContentBlock,
+  getPageHeaderBlock,
+  getPageSummeryBlock,
+  getStats,
+  getTestimonials,
+} from "@cms/content-studio";
+
+import { FAQSection } from "@layout/common/faq-section";
+import { ImageContentSection } from "@layout/common/image-content-section";
+import { MultiIMageContentVertical } from "@layout/common/multi-image-content-vertical";
+import { NewsletterSection } from "@layout/common/newsletter-section";
+import Page from "@layout/common/page";
+import { PageHeader } from "@layout/common/page-header";
+import { PageSummerySection } from "@layout/common/page-summery-section";
+import { StatsGrid } from "@layout/common/stats-grid";
+import { TestimonialSection } from "@layout/common/testimonials-section";
+
+import type { ImageContentSectionType } from "@layout/common/image-content-section";
+import type { MultiImageContentSectionType } from "@layout/common/multi-image-content-horizontal";
+import type { PageHeaderType } from "@layout/common/page-header";
+import type { PageSummerySectionType } from "@layout/common/page-summery-section";
+import type { GetStaticProps, GetStaticPropsResult, NextPage } from "next";
+import { CTASection } from "@layout/common/cta-section";
+
+interface Props {
+  header: PageHeaderType;
+  pageSummery: PageSummerySectionType;
+  lifestyleSection: MultiImageContentSectionType;
+  membershipBenefits: ImageContentSectionType;
+  membershipPerks: Stat[];
+  membershipFAQs: FAQ[];
+  testimonials: Testimonial[];
+}
+
+const AboutPage: NextPage<Props> = ({
+  header,
+  pageSummery,
+  lifestyleSection,
+  membershipBenefits,
+  membershipPerks,
+  membershipFAQs,
+  testimonials,
+}: Props): JSX.Element => {
+  return (
+    <Page title="Membership">
+      <PageHeader {...header} />
+      <PageSummerySection {...pageSummery} />
+      <MultiIMageContentVertical {...lifestyleSection} />
+      <ImageContentSection {...membershipBenefits} />
+      <StatsGrid stats={membershipPerks} />
+      <TestimonialSection testimonials={testimonials} />
+      <CTASection
+        heading="Get your membership today!"
+        subHeading="Ready to join?"
+        button={{
+          children: "Join Now",
+          route: "/join",
+          type: "route",
+          intent: "primary",
+        }}
+      />
+      <FAQSection faqs={membershipFAQs} />
+      <NewsletterSection />
+    </Page>
+  );
+};
+
+const getStaticProps: GetStaticProps = async (): Promise<
+  GetStaticPropsResult<Props>
+> => {
+  const header = await getPageHeaderBlock("7n5aipQ9O2BSEPF3nYvdd");
+  const pageSummery = await getPageSummeryBlock("5p3FcwgVIJJRzjFqx4rnQV");
+  const lifestyleSection = await getMultiImageContentBlock(
+    "0luWftpefbjFTnYPaJjAx"
+  );
+  const membershipBenefits = await getImageContentBlock(
+    "363hLOPFFvWY8bKxt0lOGG"
+  );
+
+  const membershipPerks = await getStats("membership-perks", 6);
+  const membershipFAQs = await getFAQGroup("membership-faq", 6);
+
+  const testimonials = await getTestimonials();
+
+  return {
+    props: {
+      header,
+      pageSummery,
+      lifestyleSection,
+      membershipBenefits,
+      membershipPerks,
+      membershipFAQs,
+      testimonials,
+    },
+  };
+};
+
+export { getStaticProps };
+export default AboutPage;
