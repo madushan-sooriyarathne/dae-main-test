befdedeccc1d97c5437da1dd25881cae0370fa5c
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-02-02 19:55:08 +0530

    feat: add background parallax blobs

--- src/layout/common/page-summery-section.tsx
+++ src/layout/common/page-summery-section.tsx
@@ -1,3 +1,7 @@
+import { useRef } from "react";
+import { useScroll, useTransform } from "framer-motion";
+
+import { Blob } from "@components/blob";
 import { Button } from "@components/button";
 import { PrimaryHeading } from "@components/headings/primary-heading";
 import { ImageComponent } from "@components/image-component";
@@ -16,8 +20,34 @@ const PageSummerySection: React.FC<Props> = ({
   content,
   button,
 }: Props): JSX.Element => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start center", "end center"],
+  });
+  const blobOneY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const blobTwoY = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]);
+
   return (
-    <section className="main-grid-columns grid w-full items-stretch justify-start gap-y-9 md:gap-y-12 lg:gap-y-16">
+    <section
+      className="main-grid-columns relative isolate grid w-full items-stretch justify-start gap-y-9 md:gap-y-12 lg:gap-y-16"
+      ref={ref}
+    >
+      {image && (
+        <>
+          <Blob
+            className="left-0 top-1/3 -z-10"
+            intent="primary"
+            style={{ y: blobOneY }}
+          />
+          <Blob
+            className="right-0 top-6 -z-10"
+            intent="primaryWater"
+            style={{ y: blobTwoY }}
+          />
+        </>
+      )}
+
       <div className="col-full row-start-1 row-end-2 flex w-full flex-col items-start gap-y-6 md:col-content-start-half md:pr-9 lg:pr-12 xl:pr-14 2xl:pr-16">
         <PrimaryHeading alignment="left" intent="primary">
           {heading}
