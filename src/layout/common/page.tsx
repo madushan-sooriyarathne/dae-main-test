d0d56e2901c5cfbda879999342000e865248b51f
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-02-02 19:56:03 +0530

    refactor: move nav, head & footer out of the main

--- src/layout/common/page.tsx
+++ src/layout/common/page.tsx
@@ -12,14 +12,16 @@ interface Props {
 
 const Page: React.FC<Props> = ({ title, children }: Props): JSX.Element => {
   return (
-    <main className="m-auto flex min-h-screen w-[min(100%,_208rem)] flex-col items-stretch justify-end gap-y-20 2xl:gap-y-24 4xl:gap-y-36">
+    <>
       <Head>
         <title>{`${title} - DAE Starter`}</title>
       </Head>
       <NavBar />
-      {children}
+      <main className="m-auto flex min-h-screen w-[min(100%,_208rem)] flex-col items-stretch justify-end gap-y-20 2xl:gap-y-24 4xl:gap-y-36">
+        {children}
+      </main>
       <Footer />
-    </main>
+    </>
   );
 };
 
