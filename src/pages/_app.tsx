8edb30eaa124caefd4a4f71f95739118477abf99
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-02-02 19:58:50 +0530

    style: add nextjs custom fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,22 +15,17 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { LazyMotion } from "framer-motion";
 
 const loadMotionFeatures = () =>
-  import("@styles/motion-features").then((res) => res.domAnimation);
+  import("@styles/motion-features").then((res) => res.animationFeatures);
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <LazyMotion features={loadMotionFeatures} strict>
       <QueryClientProvider client={queryClient}>
-        <style jsx global>
-          {`
-            :root {
-              --primary-font: ${plusJakartaSans.style.fontFamily};
-              --secondary-font: ${playfairDisplay.style.fontFamily};
-            }
-          `}
-        </style>
-
-        <Component {...pageProps} />
+        <div
+          className={`${plusJakartaSans.variable} ${playfairDisplay.variable} font-sans`}
+        >
+          <Component {...pageProps} />
+        </div>
         {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       </QueryClientProvider>
     </LazyMotion>
