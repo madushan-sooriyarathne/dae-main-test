15d78b4be889553092dfe5bc81a590e99008e085
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-02-03 21:20:51 +0530

    feat: add date format utility function

--- src/utils/base.ts
+++ src/utils/base.ts
@@ -21,3 +21,11 @@ export const generateRandomPath = (points: number) => {
     .join(", ");
   return path;
 };
+
+export const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
