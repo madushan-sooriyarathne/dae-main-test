9ceab1727f0d79c748ed17e8b8fb8ce586c83505
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-02-06 02:22:32 +0530

    feat: create select field component

---
+++ src/components/select-field.tsx
@@ -0,0 +1,160 @@
+import * as SelectPrimitive from "@radix-ui/react-select";
+
+import { cn } from "@lib/clsx";
+import {
+  forwardRef,
+  type ElementRef,
+  type ComponentPropsWithoutRef,
+} from "react";
+import { cva, type VariantProps } from "cva";
+
+// Root
+const Select = SelectPrimitive.Root;
+
+// Trigger
+const triggerVariants = cva(
+  [
+    "flex items-center min-h-[50px] justify-between gap-x-2 placeholder:text-base placeholder:text-white-400 border w-full rounded-sm bg-transparent px-2 py-2 font-sans text-base font-normal outline-none focus:outline focus:outline-2 outline-offset-2 placeholder:font-normal placeholder:font-sans",
+  ],
+  {
+    variants: {
+      intent: {
+        white:
+          "border-white text-white placeholder:text-white focus:outline-white ",
+        black:
+          "text-black border-black-900 placeholder:text-black-800 focus:outline-black-800",
+      },
+    },
+    defaultVariants: {
+      intent: "white",
+    },
+  }
+);
+
+interface TriggerProps
+  extends ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>,
+    VariantProps<typeof triggerVariants> {
+  placeholder?: string;
+}
+
+const SelectTrigger = forwardRef<
+  ElementRef<typeof SelectPrimitive.Trigger>,
+  TriggerProps
+>(({ className, children, placeholder, intent, ...props }, ref) => (
+  <SelectPrimitive.Trigger
+    ref={ref}
+    className={cn(triggerVariants({ intent }), className)}
+    {...props}
+  >
+    <SelectPrimitive.Value placeholder={placeholder}>
+      {children}
+    </SelectPrimitive.Value>
+    <svg className="h-4 w-4 fill-black-400">
+      <use xlinkHref="/assets/svg/sprites.svg#icon-chevron-down" />
+    </svg>
+  </SelectPrimitive.Trigger>
+));
+SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
+
+// Content Wrapper
+const SelectContent = forwardRef<
+  ElementRef<typeof SelectPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <SelectPrimitive.Portal container={document.getElementById("app")}>
+    <SelectPrimitive.Content
+      ref={ref}
+      className={cn(
+        "animate-in relative z-50 min-w-[8rem] overflow-hidden rounded-sm bg-white text-black-800 shadow-md",
+        className
+      )}
+      {...props}
+    >
+      <SelectPrimitive.Viewport className="p-1">
+        {children}
+      </SelectPrimitive.Viewport>
+    </SelectPrimitive.Content>
+  </SelectPrimitive.Portal>
+));
+SelectContent.displayName = SelectPrimitive.Content.displayName;
+
+// Option Item
+const SelectItem = forwardRef<
+  ElementRef<typeof SelectPrimitive.Item>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
+>(({ className, children, ...props }, ref) => (
+  <SelectPrimitive.Item
+    ref={ref}
+    className={cn(
+      "relative flex cursor-default select-none items-center rounded-sm py-1.5 pr-2 pl-8 font-sans text-base font-medium outline-none transition-colors hover:bg-white-100 focus:bg-black-200 data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      className
+    )}
+    {...props}
+  >
+    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+      <SelectPrimitive.ItemIndicator>
+        <svg className="h-4 w-4 fill-black-600">
+          <use xlinkHref="/assets/svg/sprites.svg#icon-checkmark" />
+        </svg>
+      </SelectPrimitive.ItemIndicator>
+    </span>
+
+    <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+  </SelectPrimitive.Item>
+));
+SelectItem.displayName = SelectPrimitive.Item.displayName;
+
+// SelectFiled
+interface Props extends ComponentPropsWithoutRef<typeof SelectTrigger> {
+  options: string[];
+  label: string;
+  name: string;
+  error?: string;
+  placeholder?: string;
+}
+
+const SelectField: React.FC<Props> = ({
+  label,
+  name,
+  intent,
+  error,
+  placeholder,
+  options,
+}: Props): JSX.Element => {
+  return (
+    <div className="flex flex-col items-start justify-start gap-y-1">
+      {label && (
+        <label
+          htmlFor={name}
+          className={`block ${
+            intent === "white" ? "text-white" : "text-black"
+          } text-sm font-semibold tracking-wider`}
+        >
+          {label}
+        </label>
+      )}
+      <Select>
+        <SelectTrigger placeholder={placeholder} />
+        <SelectContent>
+          {options.map((option) => (
+            <SelectItem
+              key={option.toLowerCase().replace(" ", "-")}
+              value={option}
+            >
+              {option}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      {error && (
+        <p className="block text-left font-sans text-xs font-medium text-primary">
+          {error}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export { SelectField };
+
+export { Select, SelectTrigger, SelectContent, SelectItem };
