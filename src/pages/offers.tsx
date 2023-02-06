5e157de3be5cf9fe7aaf80112de3e00a7d817cd0
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-02-06 18:07:58 +0530

    feat: add /offers page

---
+++ src/pages/offers.tsx
@@ -0,0 +1,70 @@
+import {
+  getOffers,
+  getPageHeaderBlock,
+  getPageSummeryBlock,
+} from "@cms/content-studio";
+
+import { NewsletterSection } from "@layout/common/newsletter-section";
+import Page from "@layout/common/page";
+import { PageHeader } from "@layout/common/page-header";
+import { PageSummerySection } from "@layout/common/page-summery-section";
+import { CTASection } from "@layout/common/cta-section";
+
+import type { PageHeaderType } from "@layout/common/page-header";
+import type { PageSummerySectionType } from "@layout/common/page-summery-section";
+import type { GetStaticProps, GetStaticPropsResult, NextPage } from "next";
+import { OfferCard } from "@layout/offers-page/offer-card";
+
+interface Props {
+  header: PageHeaderType;
+  pageSummery: PageSummerySectionType;
+  offers: Offer[];
+}
+
+const OffersPage: NextPage<Props> = ({
+  header,
+  pageSummery,
+  offers,
+}: Props): JSX.Element => {
+  return (
+    <Page title="Membership">
+      <PageHeader {...header} />
+      <PageSummerySection {...pageSummery} />
+      {offers.map((offer, index) => (
+        <OfferCard offer={offer} key={offer.id} invert={index % 2 !== 0} />
+      ))}
+
+      <CTASection
+        heading="Get your membership today!"
+        subHeading="Ready to join?"
+        button={{
+          children: "Join Now",
+          route: "/join",
+          type: "route",
+          intent: "primary",
+        }}
+      />
+
+      <NewsletterSection />
+    </Page>
+  );
+};
+
+const getStaticProps: GetStaticProps = async (): Promise<
+  GetStaticPropsResult<Props>
+> => {
+  const header = await getPageHeaderBlock("7n5aipQ9O2BSEPF3nYvdd");
+  const pageSummery = await getPageSummeryBlock("5p3FcwgVIJJRzjFqx4rnQV");
+  const offers: Offer[] = await getOffers();
+
+  return {
+    props: {
+      header,
+      pageSummery,
+      offers,
+    },
+  };
+};
+
+export { getStaticProps };
+export default OffersPage;
