e0821bf5560469ab96b5a9ea22fcd19da23bfd02
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-02-06 22:47:47 +0530

    add loading animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,12 +13,31 @@ import "mapbox-gl/dist/mapbox-gl.css";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "@lib/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { LazyMotion } from "framer-motion";
+import { AnimatePresence, LazyMotion } from "framer-motion";
+import { LoadingScreen } from "@layout/common/loading-screen";
+import { useEffect, useState } from "react";
 
 const loadMotionFeatures = () =>
   import("@styles/motion-features").then((res) => res.animationFeatures);
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleLoading = () => {
+      setLoading(false);
+    };
+
+    window.addEventListener("load", handleLoading);
+
+    const timeout = setTimeout(handleLoading, 3000);
+
+    return () => {
+      window.removeEventListener("load", handleLoading);
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <LazyMotion features={loadMotionFeatures} strict>
       <QueryClientProvider client={queryClient}>
@@ -26,6 +45,9 @@ const MyApp: AppType = ({ Component, pageProps }) => {
           className={`${plusJakartaSans.variable} ${playfairDisplay.variable} font-sans`}
           id="app"
         >
+          <AnimatePresence>
+            {loading && <LoadingScreen key="loading-screen" />}
+          </AnimatePresence>
           <Component {...pageProps} />
         </div>
         {/* <ReactQueryDevtools initialIsOpen={false} /> */}
