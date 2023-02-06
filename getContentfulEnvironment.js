fdc11ef59103fea54a73feca5e4efcd52de1b2ea
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-02-06 05:45:04 +0530

    fix: fix contentful space id env variable

--- getContentfulEnvironment.js
+++ getContentfulEnvironment.js
@@ -7,6 +7,6 @@ module.exports = function () {
   });
 
   return contentfulClient
-    .getSpace(process.env.CONTENTFUL_SPACE_ID)
+    .getSpace(process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID)
     .then((space) => space.getEnvironment("master"));
 };
