dbcbcd5aa2eca34d40d23c07ea7d3e33afddf3ae
Madushan Sooriyarathne <hello@madushan.dev>
2023-02-15 06:21:40 +0530

    refactor: finalize cta-section

--- src/layout/common/cta-section.tsx
+++ src/layout/common/cta-section.tsx
@@ -1,23 +1,34 @@
 import { Button, type ButtonType } from "@components/button";
-import { HeadingGroup, type HeadingGroupType } from "./groups/heading-group";
+import { PrimaryHeading } from "@components/headings/primary-heading";
+import { QuaternaryHeading } from "@components/headings/quaternary-heading";
 
-interface Props extends HeadingGroupType {
+interface Props {
+  heading: string;
+  subHeading?: string;
   button?: ButtonType;
 }
 
 const CTASection: React.FC<Props> = ({
   button,
-  ...headingGroupProps
+  subHeading,
+  heading,
 }: Props): JSX.Element => {
   return (
-    <section className="main-grid-columns trim-bottom grid  bg-lightWater py-12 md:py-14 lg:py-16 xl:py-24">
-      <div className="col-content flex flex-col items-center justify-start gap-y-6">
-        <HeadingGroup
-          {...headingGroupProps}
-          intent="primary"
-          alignment="center"
-        />
-        {button && <Button {...button} withArrow intent="primary" />}
+    <section className="main-grid-columns grid">
+      <div className="col-content grid w-full grid-cols-1 items-start justify-items-start gap-y-8 rounded-2xl bg-water bg-darkWater px-4 py-9 sm:px-6 md:px-8 md:py-12 mlg:grid-cols-[1fr_max-content] mlg:items-center mlg:gap-x-8 lg:py-16 xl:gap-x-16 ">
+        <div className="flex flex-col items-start justify-center">
+          {subHeading && (
+            <QuaternaryHeading alignment="left" intent="white">
+              {subHeading}
+            </QuaternaryHeading>
+          )}
+          <PrimaryHeading alignment="left" intent="white">
+            {heading}
+          </PrimaryHeading>
+        </div>
+        <div className="w-max">
+          {button && <Button {...button} withArrow solid intent="primary" />}
+        </div>
       </div>
     </section>
   );
