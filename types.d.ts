0ff58f056e1a6615e6dfb2b1c4d37a2cc1596074
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-02-16 05:42:24 +0530

    chore: add eslit escape rule

--- types.d.ts
+++ types.d.ts
@@ -2,6 +2,7 @@ import type { MultiImageContentSectionType } from "@layout/common/multi-image-co
 
 declare global {
   interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     dataLayer: any;
   }
 
